8ec69914ef13d18e22314f2a754c7bcc041d0267
Adrian <chojnacki.adrian@yahoo.com>
2020-12-19 22:29:17 +0100

    Ratios
    Values transformed to the SI units. Txt saving and loading process added

--- AAEL_Pulse_Jet/pulse_jet_engine.py
+++ AAEL_Pulse_Jet/pulse_jet_engine.py
@@ -2,18 +2,38 @@ from openpyxl import load_workbook
 import numpy as np
 import matplotlib.pyplot as plt
 
-wb = load_workbook('Pulse engine_test1_1.xlsx', read_only=True)
-ws = wb[wb.sheetnames[0]]
-data = np.array([[i.value for i in j[0:5]] for j in ws.rows])
-del ws, wb
+importData = False
+# constants
+# frequencyOfSampling, inletTemperatureToV, pressureToV, exhaustTemperatureToVo, fuelPressureToV, thrustToV,
+constants = [1/3500, 120, 120, 1.6, 1, 35]
 
+if importData:
+  # import of the data
+  wb = load_workbook('Pulse engine_test1_1.xlsx', read_only=True)
+  ws = wb[wb.sheetnames[0]]
+  data = np.array([[i.value for i in j[0:5]] for j in ws.rows])
+  names = data[0, :]
+  for columns in range(data.shape[1]):
+    savedArray = data[1:, columns].astype('float64')
+    savedArray = np.multiply(savedArray, constants[columns])
+    np.savetxt('data/'+names[columns]+'.txt', savedArray)
+  np.savetxt('data/names.txt', names, fmt='%s')
+  del ws, wb
+
+
+
+#calculating the data
+
+#
 plt.rcParams['font.size'] = '8'
 plt.rcParams['font.family'] = 'Times New Roman'
 fig, ax = plt.subplots(1, 4)
-names = data[0, :]
-
-for i in range(data.shape[1]-1):
-  ax[i].plot(data[1:, 0].astype('float64'), data[1:, i+1].astype('float64'))
-  ax[i].set_title(names[i+1])
+names = np.loadtxt('data/names.txt', delimiter='\n', dtype=str)
+time = np.loadtxt('data/'+names[0]+".txt")
+names = names[1:]
+for i in range(names.shape[0]):
+  plottedArray = np.loadtxt('data/'+names[i]+'.txt', dtype=float)
+  ax[i].plot(time, plottedArray)
+  ax[i].set_title(names[i])
 
 fig.savefig('results/plots.png')

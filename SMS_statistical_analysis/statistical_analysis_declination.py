3dad5b2aeb943d6ab399d7c1b5932ea1cac47357
Adrian <chojnacki.adrian@yahoo.com>
2020-12-20 23:09:41 +0100

    Graphs
    Fixed format of values in the arrays, corrected formula for the number of outlieres, added number of outlieres to results, decribed plots

--- SMS_statistical_analysis/statistical_analysis_declination.py
+++ SMS_statistical_analysis/statistical_analysis_declination.py
@@ -1,4 +1,5 @@
 from matplotlib import pyplot as plt
+from matplotlib import ticker
 from glob import glob
 import pandas as pd
 import numpy as np
@@ -10,7 +11,7 @@ plt.rcParams['font.size'] = '8'
 plt.rcParams['font.family'] = 'Times New Roman'
 
 #defining the names of the files in data folder
-names = glob('/SMS_statistical_analysis/data/*.*')
+names = glob('C:\GitHub\WarsawUniversityOfTechnology\SMS_statistical_analysis\data\*.*')
 
 if len(names) == 0:
   print('no data detected or wrong direction. \n Press Enter')
@@ -18,13 +19,14 @@ if len(names) == 0:
   sys.exit()
 
 #defining the required data - useful to describe resulting data
-datasets = ['No filter', 'time step 1', 'time step 10', 'time step 100']
-requiredData = ['Standard Deviation', 'Median', '25th Percentile', '75th Percentile', 'Minimum Value', 'Maximum Value']
+datasets = ['No filter', 'Time step 1', 'Time step 10', 'Time step 100']
+requiredData = ['Standard Deviation', 'Median', '25th Percentile', '75th Percentile', 'Minimum Value', 'Maximum Value',
+                'number of outliers']
 
-#creating empty libraries and lists for data
+#creating empty dictionaries and lists for data
 arrays = {}
 headings = {}
-outliers = {}
+outliers = []
 
 standardDeviation = []
 median = []
@@ -61,7 +63,7 @@ for i in range(len(names)):
     movingAverageValues.append(singularAverage)
     movingStandardDeviation.append(singularStandardDeviation)
 
-  ax[0].plot( array[:, 0])
+  ax[0].plot(heading)
   ax[1].plot(movingAverageValues)
   ax[2].plot(movingStandardDeviation)
 
@@ -72,8 +74,8 @@ for i in range(len(names)):
 # calculating required data
   median.append(np.median(heading))
   standardDeviation.append(np.std(heading))
-  quantiles_25.append(np.quantile(heading, 0.25))
-  quantiles_75.append(np.quantile(heading, 0.75))
+  quantiles_25.append(np.quantile(np.sort(heading), 0.25))
+  quantiles_75.append(np.quantile(np.sort(heading), 0.75))
 
   calculatedMinimumValues.append(quantiles_25[i] - 1.5*(quantiles_75[i]-quantiles_25[i]))
   calculatedMaximumValues.append(quantiles_25[i] + 1.5*(quantiles_75[i]-quantiles_25[i]))
@@ -82,28 +84,36 @@ for i in range(len(names)):
   outliersPosition = list(np.where(array[1] == "E")[0])
   outliersPosition += list(np.where(heading < calculatedMinimumValues[i])[0])
   outliersPosition += list(np.where(heading > calculatedMaximumValues[i])[0])
-  outliers[1] = array[outliersPosition, :]
+  outliers.append(len(outliersPosition))
 
 # updating the data and creating the histogram
   array = np.delete(array, outliersPosition, 0)
-  az.hist(array[:, 0], bins=20, alpha=0.75, histtype='barstacked')
+  az.hist(array[:, 0].astype('float64'), bins=20, alpha=0.75, histtype='barstacked')
 
 # adding the heading values to the dictionary
-  headings[datasets[i]] = array[:, 0].astype('float64')
-
-headingRange = range(int(min(minimumValues))-1, int(max(maximumValues))+1, 1)
+  headings[datasets[i]] = heading.astype('float64')
 
 # setting up the plot description
-ax[0].axes.set_yscale('linear')
-ax[0].axes.set_title('Change of heading')
-ax[1].axes.set_yscale('linear')
-ax[1].axes.set_title('Change of average heading ')
+fig1.tight_layout(pad=3.0)
+for i in range(ax.shape[0]):
+  ax[i].set_xlabel('Times [s]')
+ax[0].axes.set_yticks(np.arange(min(minimumValues), max(maximumValues), 2.5))
+ax[0].set_ylabel('Declination [$^\circ$]')
+ax[2].legend(datasets, loc='best')
+# ax[0].axes.set_title('Change of heading')
+ax[1].set_yticks(np.arange(min(minimumValues), max(maximumValues), 2.5))
+# ax[1].axes.set_title('Change of average heading ')
+ax[1].set_ylabel('Average declination [$^\circ$]')
 ax[2].axes.set_yscale('linear')
-ax[2].axes.set_title('Change of standard deviation ')
-az.axes.set_xscale('linear')
+# ax[2].axes.set_title('Change of standard deviation ')
+ax[2].set_ylabel('Standard deviation of declination')
 
-ay.boxplot(headings.values())
-
+#az.axes.set_xticks(np.arange(min(minimumValues), max(maximumValues), 2.5))
+az.axes.set_xlabel('Declination [$^\circ$]')
+az.axes.set_ylabel('Number of appearances')
+az.legend(datasets)
+ay.boxplot(headings.values(),whis=1.5,vert = False, usermedians = median,labels=datasets)
+ay.axes.set_ylabel('Declination [$^\circ$]')
 # saving the graphs
 fig1.savefig('results/heading.png')
 fig3.savefig('results/boxPlots.png')
@@ -111,5 +121,5 @@ fig2.savefig('results/histogram.png')
 
 # saving resulting data to xlsx
 data = pd.DataFrame([median, standardDeviation, quantiles_25, quantiles_75, calculatedMinimumValues,
-                     calculatedMaximumValues], columns=datasets, index=requiredData).T
+                     calculatedMaximumValues, outliers], columns=datasets, index=requiredData).T
 data.to_excel("results/results.xlsx")

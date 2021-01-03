68ea9bdddafdaccb93919cbbd5724703c0fb2992
Adrian <chojnacki.adrian@yahoo.com>
2021-01-03 13:53:46 +0100

    Pressure_Recognition
    Added file regonizing the pressure

--- AAEL_Pulse_Jet/pulse_jet_engine.py
+++ AAEL_Pulse_Jet/pulse_jet_engine.py
@@ -1,8 +1,10 @@
 from openpyxl import load_workbook
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy
 
 importData = False
+plotGraphs = False
 # constants
 # frequencyOfSampling, inletTemperatureToV, pressureToV, exhaustTemperatureToVo, fuelPressureToV, thrustToV,
 constants = [1/3500, 120, 120, 1.6, 1, 35]
@@ -20,20 +22,29 @@ if importData:
   np.savetxt('data/names.txt', names, fmt='%s')
   del ws, wb
 
-
-
 #calculating the data
 
 #
 plt.rcParams['font.size'] = '8'
 plt.rcParams['font.family'] = 'Times New Roman'
 fig, ax = plt.subplots(1, 4)
+
 names = np.loadtxt('data/names.txt', delimiter='\n', dtype=str)
 time = np.loadtxt('data/'+names[0]+".txt")
 names = names[1:]
+
 for i in range(names.shape[0]):
-  plottedArray = np.loadtxt('data/'+names[i]+'.txt', dtype=float)
-  ax[i].plot(time, plottedArray)
-  ax[i].set_title(names[i])
+  if plotGraphs:
+    plottedArray = np.loadtxt('data/'+names[i]+'.txt', dtype=float)
+    ax[i].plot(time, plottedArray)
+    frequency = scipy.fft(plottedArray)
+    ax[i].set_title(names[i])
+
+pressure_file = None
+for name in names:
+  str.lower(name)
+  if str.find(name, "chamber") > 0: pressure_file = name
+pressure = np.loadtxt('data/'+pressure_file+'.txt', dtype=float)
+pressure.
 
 fig.savefig('results/plots.png')
